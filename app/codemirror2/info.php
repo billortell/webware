1f94216088dcba95974ce2ac00bb4d9763e34b8d
eryx <evorui@gmail.com>
2011-08-26 00:48:36 +0800

    Project/UpStream: CodeMirror 2.13

---
+++ app/codemirror2/info.php
@@ -0,0 +1,11 @@
+﻿<?php
+return array (
+  'appid' => 'codemirror2',
+  'name' => 'CodeMirror 2',
+  'summary' => 'JavaScript library, online editor interface for code-like content',
+  'version' => '2.13',
+  'release' => '1',
+  'depends' => 
+  array (
+  ),
+);

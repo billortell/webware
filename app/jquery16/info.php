5db8af05ad455e819b3921cac9150cf8d6642a9b
eryx <evorui@gmail.com>
2011-08-26 00:38:23 +0800

    Project/UpStream: jQuery 1.6.2

---
+++ app/jquery16/info.php
@@ -0,0 +1,11 @@
+﻿<?php
+return array (
+  'appid' => 'jquery16',
+  'name' => 'jQuery 1.6.x',
+  'summary' => 'jQuery is a fast and concise JavaScript Library that simplifies HTML document traversing, event handling, animating, and Ajax interactions for rapid web development.',
+  'version' => '1.6.2',
+  'release' => '1',
+  'depends' => 
+  array (
+  ),
+);

21bd95e34557db97174e82eb8a015e1147a6f3d0
eryx <evorui@gmail.com>
2011-09-04 16:06:17 +0800

    Upgrade Upstream::jQuery from 1.6.2 to 1.6.3

--- app/jquery16/info.php
+++ app/jquery16/info.php
@@ -3,7 +3,7 @@ return array (
   'appid' => 'jquery16',
   'name' => 'jQuery 1.6.x',
   'summary' => 'jQuery is a fast and concise JavaScript Library that simplifies HTML document traversing, event handling, animating, and Ajax interactions for rapid web development.',
-  'version' => '1.6.2',
+  'version' => '1.6.3',
   'release' => '1',
   'depends' => 
   array (

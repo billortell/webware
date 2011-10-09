2e7357b8ad514e66c57fb3833880c33d29a23395
eryx <evorui@gmail.com>
2011-10-09 12:42:15 +0800

    Upgrade Upstream::Codemirror from 2.13 to 2.15

--- app/codemirror2/info.php
+++ app/codemirror2/info.php
@@ -3,7 +3,7 @@ return array (
   'appid' => 'codemirror2',
   'name' => 'CodeMirror 2',
   'summary' => 'JavaScript library, online editor interface for code-like content',
-  'version' => '2.13',
+  'version' => '2.15',
   'release' => '1',
   'depends' => 
   array (

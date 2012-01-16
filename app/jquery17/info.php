413f0a5cc66b8a811c38c283c57a9c7e4db8945c
eryx <evorui@gmail.com>
2012-01-16 21:10:53 +0800

    Upstream::jQuery 1.7.1 Added

---
+++ app/jquery17/info.php
@@ -0,0 +1,11 @@
+﻿<?php
+return array (
+  'appid' => 'jquery17',
+  'name' => 'jQuery 1.7.x',
+  'summary' => 'jQuery is a fast and concise JavaScript Library that simplifies HTML document traversing, event handling, animating, and Ajax interactions for rapid web development.',
+  'version' => '1.7.1',
+  'release' => '1',
+  'depends' => 
+  array (
+  ),
+);

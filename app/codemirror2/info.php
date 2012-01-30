c4725f5656ab69bd0b7bb64612830540d8022320
eryx <evorui@gmail.com>
2012-01-30 14:27:01 +0800

    Upstream::Codemirror from 2.2 to 2.21

--- app/codemirror2/info.php
+++ app/codemirror2/info.php
@@ -3,7 +3,7 @@ return array (
   'appid' => 'codemirror2',
   'name' => 'CodeMirror 2',
   'summary' => 'JavaScript library, online editor interface for code-like content',
-  'version' => '2.2',
+  'version' => '2.21',
   'release' => '1',
   'depends' => 
   array (
